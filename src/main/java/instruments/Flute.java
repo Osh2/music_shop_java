296a62ada16f6fd99db5d5b1a94b162bbaf3c00e
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-05 18:17:16 +0000

    created piano class

--- src/main/java/instruments/Flute.java
+++ src/main/java/instruments/Flute.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class Flute {
+public class Flute extends Instrument {
+
+    private String size;
+
+    public Flute(double wholesalePrice, double salePrice, String material, String type, String size) {
+        super(wholesalePrice, salePrice, material, type);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
 }
