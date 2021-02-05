3eedc1e1feed328314a5810710dcee300e5f54a7
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-05 18:20:41 +0000

    passed all flute tests

---
+++ src/test/java/instrument_tests/FluteTest.java
@@ -0,0 +1,48 @@
+package instrument_tests;
+
+import instruments.Flute;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class FluteTest {
+
+    Flute flute;
+
+    @Before
+    public void before(){
+        flute = new Flute( 80.50, 99.99, "Brass", "Standard", "Medium");
+    }
+
+    @Test
+    public void exists() {
+        assertNotNull(flute);
+    }
+
+    @Test
+    public void canGetWholesalePrice() {
+        assertEquals(80.50, flute.getWholesalePrice(),0);
+    }
+
+    @Test
+    public void canGetSalePrice() {
+        assertEquals(99.99, flute.getSalePrice(),0);
+    }
+
+    @Test
+    public void canGetMaterial() {
+        assertEquals("Brass", flute.getMaterial());
+    }
+
+    @Test
+    public void canGetType() {
+        assertEquals("Standard", flute.getType());
+    }
+
+    @Test
+    public void canGetSize() {
+        assertEquals("Medium", flute.getSize());
+    }
+}
