48e19e99c3c49b3d4d3ea94303a4681255f3f259
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-05 17:53:13 +0000

    passed first guitar test

--- src/test/java/instrument_tests/GuitarTest.java
+++ src/test/java/instrument_tests/GuitarTest.java
@@ -1,4 +1,24 @@
 package instrument_tests;
 
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar(75.73, 100.50, "Ash", "Acoustic", 6, "Wood Stain");
+    }
+
+    @Test
+    public void exists() {
+        assertNotNull(guitar);
+    }
+
+
 }
