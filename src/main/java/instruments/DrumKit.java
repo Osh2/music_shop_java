e2027e9bfa2aade953feaae59708132029dd459f
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-05 18:03:51 +0000

    created drumkit class

--- src/main/java/instruments/DrumKit.java
+++ src/main/java/instruments/DrumKit.java
@@ -1,5 +1,27 @@
 package instruments;
 
-public class
-DrumKit {
+public class DrumKit extends Instrument {
+
+    private int numToms;
+    private int numCymbals;
+    private String features;
+
+    public DrumKit(double wholesalePrice, double salePrice, String material, String type, int numToms, int numCymbals, String features) {
+        super(wholesalePrice, salePrice, material, type);
+        this.numToms = numToms;
+        this.numCymbals = numCymbals;
+        this.features = features;
+    }
+
+    public int getNumToms() {
+        return numToms;
+    }
+
+    public int getNumCymbals() {
+        return numCymbals;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
 }
