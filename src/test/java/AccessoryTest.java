5b08adabde087f65939625210bfe4f2aa9e624b3
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-05 17:37:31 +0000

    wrote accessory class and tested it exists

--- src/test/java/AccessoryTest.java
+++ src/test/java/AccessoryTest.java
@@ -6,5 +6,18 @@ import static org.junit.Assert.assertNotNull;
 
 public class AccessoryTest {
 
+    Accessory accessory;
+
+    @Before
+    public void before(){
+        accessory = new Accessory(3.40, 6.50, "5b Drum Sticks", "Vic Firth", "Percussion");
+    }
+
+    @Test
+    public void exists(){
+        assertNotNull(accessory);
+    }
+
+
 }
 
