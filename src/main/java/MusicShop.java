698d79337d2bca614f20bc138dba7e7ee0abdcc5
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-06 12:44:21 +0000

    passed first musicshop test, exists

--- src/main/java/MusicShop.java
+++ src/main/java/MusicShop.java
@@ -1,2 +1,30 @@
+import behaviours.ForSale;
+
+import java.util.ArrayList;
+
 public class MusicShop {
+
+    private ArrayList<ForSale> stock;
+    private String name;
+    private double till;
+
+    public MusicShop(String name, double till) {
+        this.stock = new ArrayList<>();
+        this.name = name;
+        this.till = till;
+    }
+
+    public ArrayList<ForSale> getStock() {
+        return stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+//    NEED TO TEST MUSIC SHOP
 }
--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class MusicShopTest {
+
+    MusicShop ricks;
+
+    @Before
+    public void before(){
+        ricks = new MusicShop("Ricks", 500);
+    }
+
+    @Test
+    public void exists() {
+        assertNotNull(ricks);
+    }
 }
